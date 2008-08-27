5b3683e6ccf2700841cf7a2f5a42ba5d9967e77f
jwight <jwight@43e68342-e247-0410-8716-5592636d597e>
2008-08-27 00:35:42 +0000

    Added.
    git-svn-id: http://touchcode.googlecode.com/svn/trunk@136 43e68342-e247-0410-8716-5592636d597e
    
    committer: jwight <jwight@43e68342-e247-0410-8716-5592636d597e>
    
    --HG--
    extra : convert_revision : d79f87f6e8688c1a9ae410e443bc6b1d1ab6b08a

---
+++ Misc/Geometry.h
@@ -0,0 +1,206 @@
+//
+//  Geometry.h
+//  SimpleSequenceGrabber
+//
+//  Created by Jonathan Wight on 10/15/2005.
+//  Copyright 2005 Toxic Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum {
+   ImageScaling_Proportionally = 0,
+   ImageScaling_ToFit,
+   ImageScaling_None,
+} EImageScaling;
+
+typedef enum {
+   ImageAlignment_Center = 0,
+   ImageAlignment_Top,
+   ImageAlignment_TopLeft,
+   ImageAlignment_TopRight,
+   ImageAlignment_Left,
+   ImageAlignment_Bottom,
+   ImageAlignment_BottomLeft,
+   ImageAlignment_BottomRight,
+   ImageAlignment_Right
+} EImageAlignment;
+
+extern CGRect ScaleAndAlignRectToRect(CGRect inImageRect, CGRect inDestinationRect, EImageScaling inScaling, EImageAlignment inAlignment);
+
+static inline Float64 RadiansToDegrees(Float64 inValue)
+{
+return(inValue * (180.0 / M_PI));
+}
+
+static inline Float64 DegreesToRadians(Float64 inValue)
+{
+return(inValue * (M_PI / 180.0));
+}
+
+static inline Float64 SemicirclesToDegrees(Float64 inValue)
+{
+return(inValue * (180.0 / pow(2.0, 31.0)));
+}
+
+static inline Float64 DegreesToSemicircles(Float64 inValue)
+{
+return(inValue * (pow(2.0, 31.0) / 180.0));
+}
+
+static inline Float64 SemicirclesToRadians(Float64 inValue)
+{
+return(DegreesToRadians(SemicirclesToDegrees(inValue)));
+}
+
+static inline Float64 RadiansToSemicircles(Float64 inValue)
+{
+return(DegreesToSemicircles(RadiansToDegrees(inValue)));
+}
+
+static inline Float64 FeetToMeters(Float64 inValue)
+{
+return(inValue * 12.0 * 2.54 / 100.0);
+}
+
+static inline Float64 MetersToFeet(Float64 inValue)
+{
+return(inValue * 100.0 / 2.54 / 12.0);
+}
+
+static inline CGPoint Rotation(CGPoint inCenter, CGFloat inAngle, CGFloat inLength)
+{
+CGFloat theCosine = cos(DegreesToRadians(fmod(90.0 - inAngle, 360.0)));
+CGFloat theSine = sin(DegreesToRadians(fmod(90.0 - inAngle, 360.0)));
+CGPoint thePoint = {
+	.x = inCenter.x + theCosine * inLength,
+	.y = inCenter.y + theSine * inLength,
+	};
+return(thePoint);
+}
+
+static inline CGRect CGRectFromPoints(CGPoint P1, CGPoint P2)
+{
+CGRect theRect = { .origin = P1, .size = { .width = fabs(P2.x - P1.x), .height = fabs(P2.y - P1.y) } };
+
+theRect.origin.x = MIN(P1.x, P2.x);
+theRect.origin.y = MIN(P1.y, P2.y);
+
+return(theRect);
+}
+
+static inline CGRect CGRectFromOriginAndSize(CGPoint inOrigin, CGSize inSize)
+{
+CGRect theRect = { .origin = inOrigin, .size = inSize };
+return(theRect);
+}
+
+static inline int quadrant(CGFloat x, CGFloat y)
+{
+if (x >= 0)
+	{
+	if (y >= 0)
+		return 0;
+	if (y < 0)
+		return 1;
+	}
+if (x < 0 && y < 0)
+	return 2;
+else
+	return 3;
+}
+
+static inline CGFloat angle(CGFloat x, CGFloat y)
+{
+const int q = quadrant(x, y);
+if (q == 0)
+	{
+	if (y == 0)
+		return 90.0;
+	return RadiansToDegrees(atan(x / y));
+	}
+else if (q == 1)
+	return 180.0 + RadiansToDegrees(atan(x / y));
+else if (q == 2)
+	return 180.0 + RadiansToDegrees(atan(x / y));
+else
+	{
+	if (x == 0.0)
+		return 0.0;
+	return 360.0 + RadiansToDegrees(atan(x / y));
+	}
+}
+
+static inline CGPoint CGPointClampToRect(CGPoint p, CGRect r)
+{
+// TODO replace with MIN and MAX
+if (p.x < CGRectGetMinX(r))
+	{
+	p.x = CGRectGetMinX(r);
+	}
+else if (p.x > CGRectGetMaxX(r))
+	{
+	p.x = CGRectGetMaxX(r);
+	}
+
+if (p.y < CGRectGetMinY(r))
+	{
+	p.y = CGRectGetMinY(r);
+	}
+else if (p.y > CGRectGetMaxY(r))
+	{
+	p.y = CGRectGetMaxY(r);
+	}
+return(p);
+}
+
+static inline CGPoint CGPointSubtract(CGPoint p1, CGPoint p2)
+{
+return(CGPointMake(p1.x - p2.x, p1.y - p2.y));
+}
+
+static inline CGFloat distance(CGPoint start, CGPoint finish)
+{
+const CGFloat theDistance = sqrtf(powf(fabsf(start.x - finish.x), 2.0) + powf(fabsf(start.y - finish.y), 2.0));
+return(theDistance);
+}
+
+static inline CGFloat magnitude(CGPoint point)
+{
+const CGFloat theMagnitude = sqrtf(fabsf(point.x * point.x) + fabsf(point.y * point.y));
+return(theMagnitude);
+}
+
+#pragma mark -
+
+struct CIntegerPoint {
+	NSInteger x;
+	NSInteger y;
+};
+typedef struct CIntegerPoint CIntegerPoint;
+
+struct CIntegerSize {
+	NSInteger width;
+	NSInteger height;
+};
+typedef struct CIntegerSize CIntegerSize;
+
+struct CIntegerRect {
+	CIntegerPoint origin;
+	CIntegerSize size;
+};
+typedef struct CIntegerRect CIntegerRect;
+
+static inline CIntegerPoint CIntegerPointMake(NSInteger x, NSInteger y)
+{
+const CIntegerPoint thePoint = { .x = x, .y = y };
+return(thePoint);
+}
+
+extern NSString *NSStringFromCIntegerPoint(CIntegerPoint inPoint);
+extern CIntegerPoint CIntegerPointFromString(NSString *inString);
+
+
+#if !defined(TARGET_OS_IPHONE) || !TARGET_OS_IPHONE
+extern NSString *NSStringFromCGAffineTransform(CGAffineTransform t);
+#endif /* defined(TARGET_OS_IPHONE) && TARGET_OS_IPHONE */
