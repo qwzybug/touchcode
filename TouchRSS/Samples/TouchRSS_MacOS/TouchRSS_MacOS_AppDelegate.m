1610ea0dcd54830a1adfeb69aa17f3f386cf3f75
Jonathan Wight <jwight@mac.com>
2009-12-04 14:50:41 -0500

    Fixed copyright.

--- TouchRSS/Samples/TouchRSS_MacOS/TouchRSS_MacOS_AppDelegate.m
+++ TouchRSS/Samples/TouchRSS_MacOS/TouchRSS_MacOS_AppDelegate.m
@@ -1,9 +1,30 @@
 //
 //  TouchRSS_MacOS_AppDelegate.m
-//  TouchRSS_MacOS
+//  TouchCode
 //
 //  Created by Jonathan Wight on 9/20/09.
-//  Copyright toxicsoftware.com 2009 . All rights reserved.
+//  Copyright 2009 toxicsoftware.com. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person
+//  obtaining a copy of this software and associated documentation
+//  files (the "Software"), to deal in the Software without
+//  restriction, including without limitation the rights to use,
+//  copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the
+//  Software is furnished to do so, subject to the following
+//  conditions:
+//
+//  The above copyright notice and this permission notice shall be
+//  included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//  OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+//  HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+//  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//  OTHER DEALINGS IN THE SOFTWARE.
 //
 
 #import "TouchRSS_MacOS_AppDelegate.h"
@@ -18,7 +39,7 @@
 - (void)dealloc
 {
 [window release];
-//	
+//
 [super dealloc];
 }
 
@@ -26,7 +47,7 @@
 {
 return([CFeedStore instance].managedObjectContext);
 }
- 
+
 - (NSUndoManager *)windowWillReturnUndoManager:(NSWindow *)window
 {
 return [[self managedObjectContext] undoManager];
@@ -35,7 +56,7 @@ return [[self managedObjectContext] undoManager];
 - (IBAction)saveAction:(id)sender {
 
     NSError *error = nil;
-    
+
     if (![[self managedObjectContext] commitEditing]) {
         NSLog(@"%@:%s unable to commit editing before saving", [self class], _cmd);
     }
@@ -53,7 +74,7 @@ NSError *theError = NULL;
 [[CFeedStore instance].feedFetcher subscribeToURL:theURL error:&theError];
 
 }
- 
+
 //- (NSApplicationTerminateReply)applicationShouldTerminate:(NSApplication *)sender {
 //
 //    if (!managedObjectContext) return NSTerminateNow;
@@ -67,16 +88,16 @@ NSError *theError = NULL;
 //
 //    NSError *error = nil;
 //    if (![managedObjectContext save:&error]) {
-//    
-//        // This error handling simply presents error information in a panel with an 
-//        // "Ok" button, which does not include any attempt at error recovery (meaning, 
-//        // attempting to fix the error.)  As a result, this implementation will 
-//        // present the information to the user and then follow up with a panel asking 
+//
+//        // This error handling simply presents error information in a panel with an
+//        // "Ok" button, which does not include any attempt at error recovery (meaning,
+//        // attempting to fix the error.)  As a result, this implementation will
+//        // present the information to the user and then follow up with a panel asking
 //        // if the user wishes to "Quit Anyway", without saving the changes.
 //
-//        // Typically, this process should be altered to include application-specific 
-//        // recovery steps.  
-//                
+//        // Typically, this process should be altered to include application-specific
+//        // recovery steps.
+//
 //        BOOL result = [sender presentError:error];
 //        if (result) return NSTerminateCancel;
 //
@@ -93,7 +114,7 @@ NSError *theError = NULL;
 //        NSInteger answer = [alert runModal];
 //        [alert release];
 //        alert = nil;
-//        
+//
 //        if (answer == NSAlertAlternateReturn) return NSTerminateCancel;
 //
 //    }
