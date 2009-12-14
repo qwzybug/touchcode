a0580b054a918978360281d4d1f7ceaefb00e0c8
Jonathan Wight <jwight@mac.com>
2009-12-14 11:53:42 -0500

    Prepping command line tool to preload certain webservice data.
    --HG--
    branch : falcon_dev

--- Support/Common/TouchcodePrefix.h
+++ Support/Common/TouchcodePrefix.h
@@ -1,9 +1,9 @@
 //
-//  NSIndexPath_Extensions.h
+//  TouchcodePrefix.h
 //  TouchCode
 //
-//  Created by Jonathan Wight on 07/23/08.
-//  Copyright 2008 toxicsoftware.com. All rights reserved.
+//  Created by Jonathan Wight on 10/15/2005.
+//  Copyright 2005 toxicsoftware.com. All rights reserved.
 //
 //  Permission is hereby granted, free of charge, to any person
 //  obtaining a copy of this software and associated documentation
@@ -27,11 +27,31 @@
 //  OTHER DEALINGS IN THE SOFTWARE.
 //
 
-#import <Foundation/Foundation.h>
+#ifdef __OBJC__
 
-@interface NSIndexPath (NSIndexPath_Extensions)
+	#if !defined(DEBUG) || DEBUG == 0
+		#define NS_BLOCK_ASSERTIONS 1
+	#endif
 
-+ (id)indexPathWithString:(NSString *)inString;
-- (NSString *)stringValue;
+	#define LOC_(key, default) \
+		[[NSBundle mainBundle] localizedStringForKey:(key) value:(default) table:NULL]
 
-@end
+	#if DEBUG == 1
+		#define Assert_(test, ...) NSAssert((test), [NSString stringWithFormat:__VA_ARGS__])
+		#define AssertC_(test, ...) NSAssertC((test), [NSString stringWithFormat:__VA_ARGS__])
+		#define AssertUnimplemented_() NSAssert(0, @"Method unimplemented")
+	#else
+		#define Assert_(test, ...) \
+			do \
+				{ \
+				if (!(test)) NSLog(__VA_ARGS__); \
+				} \
+			while (0)
+		#define AssertC_(test, ...) Assert_(test, __VA_ARGS__)
+		#define AssertUnimplemented_() NSAssert(0, @"Method unimplemented")
+	#endif
+
+    #import <Foundation/Foundation.h>
+    #import <UIKit/UIKit.h>
+	
+#endif
