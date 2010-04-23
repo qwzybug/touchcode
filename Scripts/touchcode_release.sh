0b7d37a66edcabb8e928c23560fe9696eb4504be
Jonathan Wight <jwight@mac.com>
2010-04-23 11:56:53 -0400

    Improved upload script (untested)
    --HG--
    rename : test.sh => Scripts/test.sh

--- Scripts/touchcode_release.sh
+++ Scripts/touchcode_release.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
 
-COMPONENT=TouchXML
-VERSION=1.0.7
-TAG="${COMPONENT}_${VERSION}_Release"
+COMPONENTS=( TouchXML TouchJSON TouchSQL TouchRSS )
+VERSION=1.0.8
+
 REPO="https://jwight@touchcode.googlecode.com/hg/"
-TEMP=`mktemp -d /tmp/XXXXXXX`
 #GOOGLECODEPASSWORD=
 
+TEMP=`mktemp -d /tmp/XXXXXXX`
 cd "$TEMP"
 
 echo "#### Clone fresh repo"
 hg clone -q https://touchcode.googlecode.com/hg/ touchcode
 cd "$TEMP/touchcode"
 
-echo "#### Switching to default branch"
-hg update -q -C default
+for COMPONENT in ${COMPONENTS[@]}
+do
+	echo "#### $COMPONENT"
 
-echo "#### Tagging"
-hg tag -q -f -m "Tagged ${COMPONENT} as ${TAG} for release" "${TAG}" || exit $?
+	TAG="${COMPONENT}_${VERSION}_Release"
 
-echo "#### Exporting Tag"
-hg archive -q -t "tbz2" -I "${COMPONENT}/*" -r "${TAG}" "${TEMP}/${TAG}.tar.bz2" || exit $?
+	echo "#### Switching to default branch"
+	hg update -q -C default
 
-echo "#### Pushing"
-hg push -f || exit $?
+	echo "#### Tagging"
+	hg tag -q -f -m "Tagged ${COMPONENT} as ${TAG} for release" "${TAG}" || exit $?
 
-echo "#### Uploading tarball"
-googlecode_upload.py -s "${COMPONENT} Release" -p touchcode -u "jwight" -w "${GOOGLECODEPASSWORD}" "${TEMP}/${TAG}.tar.bz2"
+	echo "#### Exporting Tag"
+	hg archive -q -t "tbz2" -I "${COMPONENT}/*" -r "${TAG}" "${TEMP}/${TAG}.tar.bz2" || exit $?
+
+	echo "#### Pushing"
+	hg push -f || exit $?
+
+	echo "#### Uploading tarball"
+	googlecode_upload.py -s "${COMPONENT} Release" -p touchcode -u "jwight" -w "${GOOGLECODEPASSWORD}" "${TEMP}/${TAG}.tar.bz2"
+
+done
 
 open "${TEMP}"
-
-
